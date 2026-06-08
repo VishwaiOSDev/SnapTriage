98dc6f443c6113b6e19443b9954b3cab825fa422
VishwaiOSDev <vishwar3001@gmail.com>
2026-06-07 22:50:41 -0400

    feat: add Spacing design tokens for grid and thumbnails

---
+++ SnapTriage/Common/DesignSystem/Spacing.swift
@@ -0,0 +1,15 @@
+//
+//  Spacing.swift
+//  SnapTriage
+//
+//  Created by Vishweshwaran on 07/06/26.
+//
+
+import CoreGraphics
+
+enum Spacing {
+    static let gridSpacing: CGFloat = 8
+    static let thumbnailMinWidth: CGFloat = 100
+    static let thumbnailCornerRadius: CGFloat = 10
+    static let thumbnailAspectRatio: CGFloat = 0.66
+}
