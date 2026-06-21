49a8a17fc24501441378e320d30a21b8ae44b117
VishwaiOSDev <vishwar3001@gmail.com>
2026-06-20 23:31:10 -0400

    Add screenshot category metadata

--- SnapTriage/Features/Triage/Model/ScreenshotCategory.swift
+++ SnapTriage/Features/Triage/Model/ScreenshotCategory.swift
@@ -18,6 +18,9 @@ enum ScreenshotCategory: String, CaseIterable, Sendable, Equatable {
     case travel
     case event
     case email
+    case identity
+    case document
+    case photo
     case other
 
     var title: String {
@@ -32,6 +35,9 @@ enum ScreenshotCategory: String, CaseIterable, Sendable, Equatable {
         case .travel:       Strings.Category.travel
         case .event:        Strings.Category.event
         case .email:        Strings.Category.email
+        case .identity:     Strings.Category.identity
+        case .document:     Strings.Category.document
+        case .photo:        Strings.Category.photo
         case .other:        Strings.Category.other
         }
     }
@@ -48,6 +54,9 @@ enum ScreenshotCategory: String, CaseIterable, Sendable, Equatable {
         case .travel:       "airplane"
         case .event:        "calendar"
         case .email:        "envelope"
+        case .identity:     "person.text.rectangle"
+        case .document:     "doc.richtext"
+        case .photo:        "photo"
         case .other:        "square.dashed"
         }
     }
